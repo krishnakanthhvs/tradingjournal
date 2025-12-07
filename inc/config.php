9e1c24cfbdf2d6dae5f21be16b41fba751ac088f
Krishna Kanth <krishnakanthhanumanthu@gmail.com>
2025-12-07 23:33:37 +0530

    Updated config file

--- inc/config.php
+++ inc/config.php
@@ -13,7 +13,7 @@ $db_name = 'trading_dashboard';
 // Detect PRODUCTION domain
 if ($host === 'mydailydiary.space' || $host === 'www.mydailydiary.space') {
     // Production DB credentials
-    $db_host = '127.0.0.1:3306';      // Change if remote
+    $db_host = '127.0.0.1';      // Change if remote
     $db_user = 'u788563593_mydailydairy';
     $db_pass = '5!7rc^Yn';
     $db_name = 'u788563593_mydailydairy';
