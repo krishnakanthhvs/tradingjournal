86bb6518a4433a94ca56a7c2598563fe8e2828f5
Krishna Kanth <krishnakanthhanumanthu@gmail.com>
2025-12-07 23:47:31 +0530

    Removed unwanted comments

--- inc/config.php
+++ inc/config.php
@@ -5,19 +5,16 @@ $host = $_SERVER['HTTP_HOST'] ?? '';
 
 // If running on live server
 if ($host === 'mydailydiary.space' || $host === 'www.mydailydiary.space') {
-
     // LIVE DATABASE
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'u788563593_mdd_user');  // change to your live DB user
-    define('DB_PASS', '7^!iT!E;n'); // change to your live DB password
-    define('DB_NAME', 'u788563593_mdd_db');     // change to your live DB name
-
+    define('DB_USER', 'u788563593_mdd_user');  
+    define('DB_PASS', '7^!iT!E;n'); 
+    define('DB_NAME', 'u788563593_mdd_db');    
 } else {
-
     // LOCAL DATABASE
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'root'); // or '' depending on MAMP
+    define('DB_PASS', 'root');
     define('DB_NAME', 'trading_dashboard');
 
 }
